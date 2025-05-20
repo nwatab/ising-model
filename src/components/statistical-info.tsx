4fa4ab2b88f2d8084be8375c051257ee87497f50
Naoki Watanabe <asterisk37n@gmail.com>
2025-05-20 12:14:07 +0900

    fix: adjust precision in ShowScientific component and improve layout in StatisticalInfo

--- src/components/statistical-info.tsx
+++ src/components/statistical-info.tsx
@@ -15,7 +15,7 @@ function ShowScientific({ value, error }: { value: number; error: number }) {
   }
   return (
     <span className="ml-1">
-      ({co.toFixed(2)}
+      ({co.toFixed(1)}
       <span className="text-xs">
         <span className="mx-1">&plusmn;</span>
         {errCo.toFixed(2)}
@@ -39,8 +39,8 @@ export default function StatisticalInfo({
   return (
     <div className="text-sm mt-8 space-y-4">
       <h2 className="text-lg font-bold mb-4">Statistics</h2>
-      <div className="flex">
-        <div className="w-36">
+      <div className="flex justify-between">
+        <div>
           Energy (<span className="italic">E</span>/
           <span className="italic">k</span>
           <sub>B</sub>
@@ -49,8 +49,11 @@ export default function StatisticalInfo({
         </div>
         <ShowScientific value={energy} error={stdevEnergy} />
       </div>
-      <div className="flex" title="±sqrt(3(T-Tc)/Tc) at T ~ Tc, h = 0">
-        <div className="w-36">
+      <div
+        className="flex justify-between"
+        title="±sqrt(3(T-Tc)/Tc) at T ~ Tc, h = 0"
+      >
+        <div>
           Magnetization (<span className="italic">M</span>):
         </div>
         <ShowScientific value={magnetization} error={stdevMagnetization} />
